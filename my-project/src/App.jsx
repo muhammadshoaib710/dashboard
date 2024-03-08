0982e3daeedde3639f5b47dccca19af2413eaf6c
SHOAIB UR REHMAN <01-135202-090@student.bahria.edu.pk>
2024-03-08 21:32:14 +0500

    new

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import Sidebar from "./sections/Sidebar";
 import Main from "./sections/Main";
 
-
-
 function App() {
   return (
     <main className="w-full bg-slate-100 h-screen flex justify-between items-start">
--- my-project/src/sections/Main.jsx
+++ my-project/src/sections/Main.jsx
@@ -11,7 +11,7 @@ import client4 from "../assets/images/client4.jpg";
 
 function Main() {
   return (
-    <section className="w-4/5 grow bg-white h-screen overflow-y-auto flex flex-row justify-start items-center gap-2 p-4">
+    <section className="w-2/5  grow bg-white h-screen overflow-y-auto flex flex-col justify-start items-center gap-2 p-4">
       <Header />
       <div
         id="main-section"
@@ -21,21 +21,18 @@ function Main() {
           id="left"
           className="col-span-2 p-2 gap-3 flex flex-col justify-between item-center h-full"
         >
-          <div className="grid lg-grid-col-3 grid-cols-1 w-full mb-4">
-            <div
-              className="w-full flex flex-col justify-center items-center bg-blue-200 p-5 rounded-xl gap-5 transition-transform
-            transform hover:rotate-[-5deg] hover:scale-105 cursor-pointer"
-            >
-              <div className="w-full flex justify-between items-center"> 
+          <div className="grid lg-grid-cols-3 grid-cols-1 gap-4 w-full mb-4">
+            <div className="w-1/4 flex flex-col justify-center items-center bg-blue-200 p-5 rounded-xl gap-5 transition-transform transform hover:rotate-[-5deg] hover:rotate[-5deg] cursor-pointer">
+              <div className="w-full flex justify-between items-center">
                 <h1 className="text-md text-black font-semibold">Facebook</h1>
                 <h1 className=" text-green-600 font-semibold">+21.75%</h1>
               </div>
               <div className="w-full flex justify-between items-center">
                 <div className="flex flex-col justify-center items-start gap-1">
-                  <h1 className="text-3xl text-black font-semibold ">10,328
+                  <h1 className="text-3xl text-black font-semibold ">
+                    10,328
                     <p className="text-slate-700">Followers</p>
                   </h1>
-                  
                 </div>
               </div>
             </div>
