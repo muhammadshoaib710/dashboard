76f6878c39b6d62dc46691baf2c3053ce672bce4
SHOAIB UR REHMAN <01-135202-090@student.bahria.edu.pk>
2024-03-08 22:03:47 +0500

    Update Messages component to display content

--- my-project/src/sections/Messages.jsx
+++ my-project/src/sections/Messages.jsx
@@ -3,10 +3,7 @@ import Header from "../components/Header";
 
 function Messages() {
     return (  
-        <><Header /><div>
-            <h1>Messages</h1>
-            <p>This is where you can view all your messages</p>
-        </div></>
+        <div>Content goes here</div>
     );
-    }
+}
 export default Messages; 
